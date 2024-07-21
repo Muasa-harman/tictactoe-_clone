fd71014c37002022b3bf7ae8e8453eb7ffe2a467
Muasa-harman <harman.muasa@gmail.com>
2024-07-21 15:51:34 +0300

    init

---
+++ tictatoe.py
@@ -0,0 +1,53 @@
+import sys
+import pygame
+
+from const import *
+
+# PYGAME
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+class Game:
+
+    
+    def __init__(self):
+        self.board
+
+
+    def show_lines(self):
+
+        # vertical
+        pygame.draw.line(screen, LINE)
+
+
+
+    def draw_fig(self, row,cols):
+        # if self.player = self.player % 2 + 1
+
+            
+
+    def main():
+
+        board = game.board
+        game = Game()
+        while True:
+         for event in pygame.event.get():
+
+            if event.type == pygame.QUITE:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.
+                pos = event.pos   
+                row = pos[1] //SIQSIC
+                col = pos[0] //SIQSIC 
+
+
+class Board:
+      def __init__(self) -> None:
+            
+            print(self.squares)
+
+
+     def empty_sqr():       
